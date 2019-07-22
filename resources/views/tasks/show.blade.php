f769d527857c97148947ef51fd0ec07188e45ec3
yuto.nakada <dondoruma.net@gmail.com>
2019-07-22 13:41:54 +0000

    fix show.blade.php

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -14,5 +14,11 @@
             <td>{{ $task->content }}</td>
         </tr>
     </table>
+    
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 
 @endsection
