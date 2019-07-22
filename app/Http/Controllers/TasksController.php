d1ba2afed6e588e31c2be907d66db7ae6a58a423
yuto.nakada <dondoruma.net@gmail.com>
2019-07-22 12:52:37 +0000

    CRUD tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    // getでmessages/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+        
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    // postでmessages/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    // getでmessages/idにアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // getでmessages/id/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // putまたはpatchでmessages/idにアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    // deleteでmessages/idにアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect('/');
+    }
+}
