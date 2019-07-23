395733be7cef1de518f5ee950e0842e20a54f9da
yuto.nakada <dondoruma.net@gmail.com>
2019-07-23 13:07:39 +0000

    change migration

--- database/migrations/2019_07_23_012334_add_status_to_tasks_table.php
+++ database/migrations/2019_07_23_012334_add_status_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToTasksTable extends Migration
+class AddStatusToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddTitleToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10)->change();
+            $table->string('status', 10);
         });
     }
 
