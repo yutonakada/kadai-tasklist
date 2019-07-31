8fce6b31c3fe435cec1e95edc188cd39d90c8e79
yuto.nakada <dondoruma.net@gmail.com>
2019-07-31 11:47:02 +0000

    migration key

--- database/migrations/2019_07_22_141037_create_tasks_table.php
+++ database/migrations/2019_07_22_141037_create_tasks_table.php
@@ -18,6 +18,9 @@ class CreateTasksTable extends Migration
             $table->string('content');
             $table->timestamps();
         });
+        
+        // 外部キー制約
+        $table->foreign('user_id')->references('id')->on('users');
     }
 
     /**
